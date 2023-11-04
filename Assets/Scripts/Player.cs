a67c2e7e19de811ef268cf849cbdc3bd5383f499
leonnik28 <lencruseckij@gmail.com>
2023-11-04 12:22:57 +0300

    Joystick and JumpButton create

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private Movement _movement;
 
-    private float _horizontalInput;
+    private Vector2 m_move;
 
     private void Update()
     {
-        _movement.Move(_horizontalInput);
+        _movement.Move(m_move.x);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        m_move = value.Get<Vector2>();
+    }
+
+    private void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            _movement.Jump();
+        }
     }
 }
